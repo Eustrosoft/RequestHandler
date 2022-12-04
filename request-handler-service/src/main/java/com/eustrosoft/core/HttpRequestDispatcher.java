548b6aaea1fc3cde1ad7bebfbd23e53050797585
Yadzuka <yadzuka@eustrosoft.org>
2022-12-04 12:03:11 +0300

    Added startup servlet & some settings

--- request-handler-service/src/main/java/com/eustrosoft/core/HttpRequestDispatcher.java
+++ request-handler-service/src/main/java/com/eustrosoft/core/HttpRequestDispatcher.java
@@ -1,8 +1,27 @@
 package com.eustrosoft.core;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(
+        name = "EustrosoftRequestDispatcher",
+        description = "Dispatches request depending on it's body dispatch value",
+        urlPatterns = {"/dispatch"}
+)
 public class HttpRequestDispatcher extends HttpServlet {
 
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
 
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello world!");
+        writer.flush();
+        writer.close();
+    }
 }
---
+++ request-handler-service/src/main/java/com/eustrosoft/core/WelcomeServlet.java
@@ -0,0 +1,24 @@
+package com.eustrosoft.core;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        urlPatterns = {"."}
+)
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        PrintWriter writer = resp.getWriter();
+        writer.println("You are alive!");
+        writer.flush();
+        writer.close();
+    }
+}
