ee3a2359f12f5836addb7f4bd86f5a4018693439
Yadzuka <yadzuka@eustrosoft.org>
2023-06-04 20:42:28 +0300

    Params updated

--- request-handler-service/src/main/java/com/eustrosoft/core/tools/WebParams.java
+++ request-handler-service/src/main/java/com/eustrosoft/core/tools/WebParams.java
@@ -4,8 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 public final class WebParams {
     public final static String DB_URL = "dbUrl";
-    public final static String DB_POOL_URL = "QDBPOOL_NAME";
-    public final static String DB_POOL_NAME = "QDBDPOOL_NAME";
+    public final static String DB_POOL_URL = "QDBPOOL_URL";
+    public final static String DB_POOL_NAME = "QDBPOOL_NAME";
     public final static String DB_JDBC_CLASS = "QDBPOOL_JDBC_CLASS";
     public final static String DEBUG = "debug";
 
