c665e58f4de0d142f1f022f374c7c893118ebbf0
Yadzuka <yadzuka@eustrosoft.org>
2023-09-26 23:47:25 +0300

    Deploy a bit changed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 cd request-handler-service/
 mvn clean package
 scp -r target/classes/com/ yadzuka@fudo.eustrosoft.org:/home/yadzuka/workspace/
-#ssh yadzuka@fudo.eustrosoft.org 'scp -r /home/yadzuka/workspace/eustrosoft /usr/local/apache-tomcat-9.0/webapps/eustrosofthandler_war/WEB-INF/classes/'
-#ssh yadzuka@fudo.eustrosoft.org 'rm -rf /usr/local/apache-tomcat-9.0/webapps/eustrosofthandler_war/WEB-INF/classes/com/'
-#ssh yadzuka@fudo.eustrosoft.org 'mkdir /usr/local/apache-tomcat-9.0/webapps/eustrosofthandler_war/WEB-INF/classes/com'
-#ssh yadzuka@fudo.eustrosoft.org 'mv /usr/local/apache-tomcat-9.0/webapps/eustrosofthandler_war/WEB-INF/classes/eustrosoft /usr/local/apache-tomcat-9.0/webapps/eustrosofthandler_war/WEB-INF/classes/com'
